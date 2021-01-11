23199bb93f6ccbf04a6b3761afc81972f79bfa53
YannickNaumann <yannick.naumann@tu-dortmund.de>
2021-01-11 19:35:25 +0100

    ScoreTest test for compareTo and Equals

--- tests/model/ScoreTest.java
+++ tests/model/ScoreTest.java
@@ -129,6 +129,32 @@ public class ScoreTest {
         assertNotEquals(System.identityHashCode(testScore),System.identityHashCode(testScore.clone()));
     }
 
+    /**
+     * Test compareTo
+     */
+    @Test
+    public void testCompareTo(){
+        Score testScoreSmall = new Score(1, LocalDateTime.now(),false,PlayerType.AI_MEDIUM,"Peter");
+        Score testScoreLarge = new Score(2, LocalDateTime.now(),false,PlayerType.AI_MEDIUM,"Peter");
+        Score testScoreLarge2= new Score(2, LocalDateTime.now(),false,PlayerType.AI_MEDIUM,"Peter");
+
+        assertTrue(testScoreSmall.compareTo(testScoreLarge) < 0);
+        assertTrue(testScoreLarge.compareTo(testScoreSmall) > 0);
+        assertTrue(testScoreLarge.compareTo(testScoreLarge2) == 0);
+
+
+    }
+
+    /**
+     * Test NotEquals
+     */
+    @Test
+    public void testNotEquals(){
+        Score testScoreSmall = new Score(1, LocalDateTime.now(),false,PlayerType.AI_MEDIUM,"Peter");
+        Score testScoreLarge = new Score(2, LocalDateTime.now(),false,PlayerType.AI_MEDIUM,"Peter");
+        assertNotEquals(testScoreLarge,testScoreSmall);
+    }
+
 
 
 }
