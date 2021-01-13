8d6cb612b02a24df3f903f78c20933341dbf647b
Abdullah Ourfali <abdullah.ourfali@tu-dortmund.de>
2021-01-13 09:54:00 +0100

    implementation of Game

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,37 +1,102 @@
 package model;
 
-import java.util.List;
-
+/**
+ * @author Abdullah Ourfali
+ * Represents the Game
+ */
 public class Game {
-
+	/**
+	 * Currently state of the game
+	 */
 	private int currentGameState;
 
+	/**
+	 * The highscore is reachable
+	 */
 	private boolean highscoreReachable;
 
+	/**
+	 * Is ironman mode
+	 */
 	private final boolean ironman;
 
+	/**
+	 * The simulated speed
+	 */
 	private int simulationSpeed;
 
-	private List<GameState> gameState;
+	/**
+	 * All states of the game
+	 */
+	private GameState[] gameState;
 
+	/**
+	 *Initializes a new Game
+	 * @param ironman the mode of the new game
+	 */
 	public Game(boolean ironman) {
 		this.ironman = ironman;
 	}
 
-	public void addGameState(GameState gameState) {
-
-	}
-
-	@Override
-	public Game clone() {
-		return null;
-	}
-
-	public int getCurrentGameState() {
+	/**
+	 * Gets the currently state of the game
+	 * @return the currently state
+	 */
+	public int getCurrentGameState(){
 		return currentGameState;
 	}
 
-	public List<GameState> getGameStates() {
-		return gameState;
+	/**
+	 * Gets the reachabled highscore
+	 * @return the reachabled highscore
+	 */
+	public boolean getHighscoreReachable(){
+		return highscoreReachable;
 	}
+
+	/**
+	 * Gets the game mode
+	 * @return the ironman mode
+	 */
+	public boolean getIronman(){
+		return ironman;
+	}
+
+	/**
+	 * Gets the simulated speed
+	 * @return simulated speed
+	 */
+	public int getSimulationSpeed(){
+		return simulationSpeed;
+	}
+
+	/**
+	 * Gets all states of the game
+	 * @return all game states
+	 */
+	public GameState[] getGameState(){ return gameState; }
+
+	/**
+	 * Adds the state in parameter at last index
+	 * @param gameState the new state
+	 */
+	public void addGameState(GameState gameState) {
+		int i = this.gameState.length + 1;
+		this.gameState[i] = gameState;
+	}
+
+	/**
+	 * Clones the object
+	 * @return Copy of the object
+	 */
+	@Override
+	public Game clone() {
+		Game game;
+		try { return game = (Game)super.clone(); }
+		catch (CloneNotSupportedException e) {
+			System.out.println (" Cloning not allowed. " );
+			return null;
+		}
+	}
+
 }
--- src/model/Player.java
+++ src/model/Player.java
@@ -16,6 +16,9 @@ public class Player {
 	public Player(Integer boardPosition, Integer money, String name) {
 	}
 
+	public Player(Player player) {
+	}
+
 	@Override
 	public Player clone() {
 		return null;
