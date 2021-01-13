271d86aeca0e2ba188c0bef0b8face4614d472b1
Mohammed <mohammed.osman@tu-dortmund.de>
2021-01-13 15:24:28 +0100

    QuiltBoard model finished

--- src/model/QuiltBoard.java
+++ src/model/QuiltBoard.java
@@ -1,16 +1,63 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class QuiltBoard {
 
+	/**
+	 * A 2D Array for the player's quilt board
+	 */
 	private int[][] patchBoard;
 
-	private List<Patch> patch;
+	/**
+	 * A list of the patches on the quilt board
+	 */
+	private List<Patch> patches;
 
-	@Override
-	public QuiltBoard clone() {
-		return null;
+	/**
+	 * Constructor for the class QuiltBoard
+	 * Sets the size of the board to 9x9
+	 */
+	public QuiltBoard(){
+		patchBoard = new int[9][9];
+		patches = new ArrayList<Patch>();
 	}
 
+
+	/**
+	 * Clones the quilt board
+	 * @return A copy of the quilt board
+	 */
+	@Override
+	public QuiltBoard clone() {
+		QuiltBoard clonedBoard = new QuiltBoard();
+		clonedBoard.setPatches(this.patches);
+		return clonedBoard;
+	}
+
+
+	/**
+	 * Adds a new patch onto the quilt board
+	 * @param patch The patch that will be added
+	 */
+	public void addPatch(Patch patch){
+		patches.add(patch);
+	}
+
+	/**
+	 * Getter for the list of patches on the quilt board
+	 * @return the list of patches
+	 */
+	public List<Patch> getPatches(){
+		return patches;
+	}
+
+	/**
+	 * Setter for the list of patches on the quilt board
+	 * @param patchList
+	 */
+	public void setPatches(List<Patch> patchList){
+		this.patches = patchList;
+	}
 }
