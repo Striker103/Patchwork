e4410a0fa34fc71a359b75921920f6675b8a8cd8
Alexandra Latys <alexandra.latys@tu-dortmund.de>
2021-01-18 00:20:44 +0100

    Add saveGame

--- src/controller/IOController.java
+++ src/controller/IOController.java
@@ -1,17 +1,23 @@
 package controller;
 
 
+import com.google.gson.Gson;
+import model.CheckUtil;
+import model.Game;
 import model.Patch;
 import view.aui.ErrorAUI;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class IOController {
 
-	private MainController mainController;
+	private final MainController mainController;
 
-	private ErrorAUI errorAUI;
+	private final ErrorAUI errorAUI;
 
 	/**
 	 * Constructor that sets the mainController and all AUIs
@@ -23,7 +29,31 @@ public class IOController {
 		this.errorAUI = errorAUI;
 	}
 
-	public void saveGame() {
+	/**
+	 * Saves the game to the specified file
+	 * @param file file
+	 */
+	public void saveGame(File file) {
+		Game game = mainController.getGame();
+
+		CheckUtil.assertNonNull(game, file);
+
+		if (file.isDirectory() || !file.canWrite()) {
+			errorAUI.showError("File is invalid");
+			return;
+		}
+
+		Gson gson = new Gson();
+		String json = gson.toJson(game);
+
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			writer.write(json);
+
+			writer.close();
+		} catch (IOException exception) {
+			errorAUI.showError("IOException occurred.");
+		}
 
 	}
 
@@ -36,6 +66,18 @@ public class IOController {
 	}
 
 	public List<Patch> importCSV(File file) {
+
+		//		List<List<String>> records = new ArrayList<>();
+//		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+//			String line;
+//			while ((line = br.readLine()) != null) {
+//				String[] values = line.split(COMMA_DELIMITER);
+//				records.add(Arrays.asList(values));
+//			}
+//		} catch (IOException e){
+//			errorAUI.showError("IOException occurred.");
+//			return null;
+//		}
 		return null;
 	}
 
