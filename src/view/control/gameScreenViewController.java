c16b24c0c9982e1289b1a2ed787fc5aad072d72f
Mohammed <mohammed.osman@tu-dortmund.de>
2021-01-17 22:40:27 +0100

    added the gameScreen fxml file

---
+++ src/view/control/gameScreenViewController.java
@@ -0,0 +1,20 @@
+package view.control;
+
+import javafx.event.ActionEvent;
+
+public class gameScreenViewController {
+    public void onPauseButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void onHintButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void onUndoButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void onRedoButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void onPassButtonAction(ActionEvent actionEvent) {
+    }
+}
