fe38fa70e523b210f0e29e885929f0821b4b131d
Alexandra Latys <alexandra.latys@tu-dortmund.de>
2021-01-18 11:39:45 +0100

    Move specialTileAttribute

--- src/model/GameState.java
+++ src/model/GameState.java
@@ -10,10 +10,6 @@ import java.util.stream.Collectors;
  * GameState is an Object which holds all informations regarding the current player move. Every player move has its own GameState
  */
 public class GameState{
-	/**
-	 * checks if the one and only Special Tile is still available
-	 */
-	private boolean specialTileAvailable;
 
 	/**
 	 * protocol for all changes at each move
@@ -45,13 +41,12 @@ public class GameState{
 	 *
 	 * @param playerNames 			names and types of players
 	 * @param patches 				list of patches
-	 * @param specialTileAvailable	true if the bonus tile is still available
 	 *
 	 * @throws IllegalArgumentException when player name is empty
 	 */
-	public GameState(Tuple<Tuple<String, PlayerType>, Tuple<String, PlayerType>> playerNames, List<Patch> patches, boolean specialTileAvailable)
+	public GameState(Tuple<Tuple<String, PlayerType>, Tuple<String, PlayerType>> playerNames, List<Patch> patches)
 	{
-		CheckUtil.assertNonNull(playerNames, patches, specialTileAvailable);
+		CheckUtil.assertNonNull(playerNames, patches);
 		CheckUtil.assertNonNull(playerNames.getFirst(), playerNames.getSecond());
 
 		Tuple<String, PlayerType> firstPlayerNameAndType = playerNames.getFirst();
@@ -60,7 +55,6 @@ public class GameState{
 		if(firstPlayerNameAndType.getFirst().isBlank() || secondPlayerNameAndType.getFirst().isBlank())
 			throw new IllegalArgumentException("At least one player name is missing");
 
-		this.specialTileAvailable = specialTileAvailable;
 
 
 		player1 = new Player(0, 5, firstPlayerNameAndType.getFirst(), firstPlayerNameAndType.getSecond());
@@ -78,15 +72,13 @@ public class GameState{
 	/**
 	 * Constructor for clones
 	 *
-	 * @param specialTileAvailable	true if the bonus tile is still available
 	 * @param logEntry				change at move
 	 * @param timeBoard				timeBoard Array
 	 * @param patches				list of patches
 	 * @param player1				first player
 	 * @param player2				second player
 	 */
-	private GameState(boolean specialTileAvailable, String logEntry, TimeBoardComponent[] timeBoard, List<Patch> patches, Player player1, Player player2) {
-		this.specialTileAvailable = specialTileAvailable;
+	private GameState(String logEntry, TimeBoardComponent[] timeBoard, List<Patch> patches, Player player1, Player player2) {
 		this.logEntry = logEntry;
 		TimeBoardComponent[] arr = new TimeBoardComponent[timeBoard.length];
 		for (int i = 0; i < timeBoard.length; i++) {
@@ -116,8 +108,7 @@ public class GameState{
 		if (obj.getClass() == this.getClass())
 		{
 			GameState gameState = (GameState) obj;
-			return 	specialTileAvailable == gameState.specialTileAvailable &&
-					logEntry.equals(gameState.logEntry) &&
+			return logEntry.equals(gameState.logEntry) &&
 					Arrays.equals(timeBoard, gameState.timeBoard) &&
 					patches.equals(gameState.patches) &&
 					player1.equals(gameState.player1) &&
@@ -131,7 +122,7 @@ public class GameState{
 	 * @return A copy of the GameState object
 	 */
 	public GameState copy() {
-		return new GameState(specialTileAvailable, logEntry, timeBoard, patches, player1, player2);
+		return new GameState( logEntry, timeBoard, patches, player1, player2);
 	}
 
 	/**
@@ -140,11 +131,6 @@ public class GameState{
 	 */
 	public void setLogEntry(String logEntry){this.logEntry = logEntry;}
 
-	/**
-	 * changes status of special tile availability
-	 * @param specialTileAvailable info if tile is available
-	 */
-	public void setSpecialTileAvailable(boolean specialTileAvailable) { this.specialTileAvailable = specialTileAvailable; }
 
 	/**
 	 * changes the patches List
@@ -152,11 +138,6 @@ public class GameState{
 	 */
 	public void setPatches(List<Patch> patches) { this.patches = patches; }
 
-	/**
-	 * Checks if special tile is still available
-	 * @return true if special tile is still available
-	 */
-	public boolean getSpecialTileAvailable() {return specialTileAvailable;}
 
 	/**
 	 * returns the information of all actions made at this GameState
