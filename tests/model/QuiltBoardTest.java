dd6b8e95f811dd01e967721407cc0bd4f514ff9a
Maya Samha <maya.samha@tu-dortmund.de>
2021-01-20 09:17:45 +0100

    QuiltBoardTest is now completely tested and fixed the size of shape

--- tests/model/QuiltBoardTest.java
+++ tests/model/QuiltBoardTest.java
@@ -1,6 +1,7 @@
 package model;
 
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -17,16 +18,31 @@ public class QuiltBoardTest{
     private final int INCOME = 1;
     private final int BUTTONCOST = 3;
     private final boolean[][] SHAPE = new boolean[][]{
-            {true, true, false, false},
-            {true, true, true, false},
-            {false, false, false, false},
-            {false, false, false, false}};
+            {true, true, false, false, false},
+            {true, true, true, false, false},
+            {false, false, false, false, false}};
+
     private final int TIME = 3;
 
     private final Patch PATCH = new Patch(ID, INCOME, BUTTONCOST, new Matrix(SHAPE), TIME);
 
+    Matrix placementMatrix;
+
+
+    @Before
     public void setUp() {
         quiltBoard = new QuiltBoard();
+        boolean[][] placement = new boolean[9][9];
+        int xPos=0;
+        int yPos=0;
+        for(int i= xPos; i<3; i++){
+            for(int j= yPos; j<5; j++){
+                if(SHAPE[i][j]){
+                    placement[i][j]=true;
+                }
+            }
+        }
+        placementMatrix=new Matrix(placement);
 
     }
 
@@ -43,8 +59,25 @@ public class QuiltBoardTest{
      */
     @Test
     public void testAddPatch() {
-       // quiltBoard.addPatch(PATCH);
-        //assertTrue(quiltBoard.getPatches().get(0).equals(PATCH));
+        quiltBoard.addPatch(PATCH, placementMatrix, 0, false);
+        assertTrue(quiltBoard.getPatches().get(0).equals(PATCH));
+        assertTrue(quiltBoard.getPatchBoard().equals(placementMatrix));
+    }
+    /**
+     * Tests addPatch with 2 overlapping patches
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddPatch2OnSamePosition(){
+        quiltBoard.addPatch(PATCH, placementMatrix, 0, false);
+        quiltBoard.addPatch(PATCH, placementMatrix, 0, false);
+    }
+
+    /**
+     * Tests addPatch with an invalid angle
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddPatchInvalidAngle(){
+        quiltBoard.addPatch(PATCH, placementMatrix, 15, false);
     }
 
     /**
@@ -52,10 +85,30 @@ public class QuiltBoardTest{
      */
     @Test
     public void testGetPatches() {
-       // quiltBoard.addPatch(PATCH);
+        quiltBoard.addPatch(PATCH, placementMatrix, 0, false);
         List<Patch> patches = new ArrayList<>();
         patches.add(PATCH);
         assertEquals(quiltBoard.getPatches(), patches);
     }
+    /**
+     * Tests getPatchBoard
+     */
+    @Test
+    public void testGetPatchBoard(){
+        quiltBoard.addPatch(PATCH, placementMatrix, 0, false);
+        assertEquals(quiltBoard.getPatchBoard(), placementMatrix);
+    }
 
+
+
+    /**
+     * Tests notEquals
+     */
+    @Test
+    public void testNotEquals(){
+        QuiltBoard testBoard1 = new QuiltBoard();
+        testBoard1.addPatch(PATCH, placementMatrix, 0, false);
+        QuiltBoard testBoard2 = new QuiltBoard();
+        assertNotEquals(testBoard1, testBoard2);
+    }
 }
