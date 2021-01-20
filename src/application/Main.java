878375ca74e474f0403f7e79e86b4456aed9b6d4
Mohammed <mohammed.osman@tu-dortmund.de>
2021-01-20 22:07:58 +0100

    Added MainViewController and adjusted some view controllers

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,15 +1,21 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import view.SampleViewController;
+import view.control.MainViewController;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
+	public void start(Stage primaryStage) throws Exception{
+		/*try {
 			SampleViewController sampleViewController = new SampleViewController();
 			Scene scene = new Scene(sampleViewController,400,200);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
@@ -17,7 +23,18 @@ public class Main extends Application {
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
-		}
+		}*/
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/fxml/StartGame.fxml"));
+
+		Pane root = loader.load();
+		MainViewController controller = loader.getController();
+
+		controller.setPrimaryStage(primaryStage);
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		controller.setMainMenuNode(scene);
+		controller.initialize();
 	}
 	
 	public static void main(String[] args) {
---
+++ src/view/control/MainViewController.java
@@ -0,0 +1,202 @@
+package view.control;
+
+import controller.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import model.Game;
+import model.Patch;
+import model.Score;
+import view.aui.*;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainViewController {
+
+    private double oldHeight = 800, oldWidth = 600;
+    private MainController mainController;
+
+    @FXML
+    private Button newGameButton;
+
+    private Parent ownNode;
+
+    private GameScreenViewController gameScreenViewController;
+    private GameSummaryViewController gameSummaryViewController;
+    private HighscoresViewController highscoresViewController;
+    private LoadGameViewController loadGameViewController;
+    private NewGameViewController newGameViewController;
+    private PauseGameViewController pauseGameViewController;
+
+
+    private Stage primaryStage;
+    private Scene mainMenuScene;
+    private Scene currentScene;
+
+
+
+    private HighscoreAUI highscoreAUI = new HighscoreAUI() {
+        @Override
+        public void showHighscores(List<Score> highscores) {
+
+        }
+    };
+
+    private ErrorAUI errorAUI = new ErrorAUI() {
+        @Override
+        public void showError(String message) {
+
+        }
+    };
+
+    private HintAUI hintAUI = new HintAUI() {
+        @Override
+        public void showHintAdvance() {
+
+        }
+
+        @Override
+        public void showHintTakePatch(Patch patch, boolean[][] placing) {
+
+        }
+    };
+
+    private LogAUI logAUI = new LogAUI() {
+        @Override
+        public void updateLog(String log) {
+
+        }
+    };
+
+    private TurnAUI turnAUI = new TurnAUI() {
+        @Override
+        public void triggerPlayerTurn() {
+
+        }
+    };
+
+    private LoadGameAUI loadGameAUI = new LoadGameAUI() {
+        @Override
+        public void loadGame(List<Game> games) {
+
+        }
+    };
+
+    public MainViewController(){
+        mainController = new MainController(highscoreAUI, errorAUI, hintAUI, logAUI, turnAUI, loadGameAUI);
+    }
+
+    public MainController getMainController(){
+        return mainController;
+    }
+
+    public Scene getMainMenuNode() {
+        return mainMenuScene;
+    }
+
+    public void setMainMenuNode(Scene mainMenuScene) {
+        this.mainMenuScene = mainMenuScene;
+    }
+
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public Scene getCurrentScene() {
+        return currentScene;
+    }
+
+    public void setCurrentScene(Scene currentScene) {
+        this.currentScene = currentScene;
+    }
+
+    public void initialize() throws IOException {
+        if (primaryStage != null) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/fxml/StartGame.fxml"));
+            BorderPane root = loader.load();
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+            currentScene = scene;
+
+
+            FXMLLoader newGameLoader = new FXMLLoader(getClass().getResource("/view/fxml/newGame.fxml"));
+            Pane newGameRoot = newGameLoader.load();
+            newGameViewController = newGameLoader.getController();
+            newGameViewController.setMainViewController(this);
+            //scene = new Scene(newGameRoot);
+            newGameViewController.setOwnScene(new Scene(newGameRoot));
+
+
+            FXMLLoader loadGameLoader = new FXMLLoader(getClass().getResource("/view/fxml/LoadGame.fxml"));
+            Pane loadGameRoot = loadGameLoader.load();
+            loadGameViewController = loadGameLoader.getController();
+            loadGameViewController.setMainViewController(this);
+            //scene = new Scene(loadGameRoot);
+            loadGameViewController.setOwnScene(new Scene(loadGameRoot));
+
+
+            FXMLLoader highscoreLoader = new FXMLLoader(getClass().getResource("/view/fxml/highscores.fxml"));
+            Pane highscoreRoot = highscoreLoader.load();
+            highscoresViewController = highscoreLoader.getController();
+            highscoresViewController.setMainViewController(this);
+            highscoresViewController.setOwnScene(new Scene(highscoreRoot));
+
+
+            showCurrentScene();
+        }
+
+
+
+        /*this.primaryStage.setMinHeight(600 + (this.primaryStage.getHeight() - this.primaryStage.getScene().getHeight()));
+        System.out.println(this.primaryStage.getMinHeight());
+        this.primaryStage.setMinWidth(800);*/
+    }
+
+    private void showScene() {
+        setCurrentScene(currentScene);
+        showCurrentScene();
+    }
+
+    public void showCurrentScene(){
+        if(primaryStage != null) {
+            primaryStage.setScene(currentScene);
+            primaryStage.setWidth(oldWidth);
+            primaryStage.setHeight(oldHeight);
+            primaryStage.setTitle("SoPra");
+            primaryStage.show();
+        }
+    }
+
+    private void showMainNode() {
+        currentScene = mainMenuScene;
+        showCurrentScene();
+    }
+
+    @FXML
+    public void onNewGameAction(ActionEvent actionEvent) {
+        newGameViewController.showScene();
+    }
+
+    @FXML
+    public void onLoadGameAction(ActionEvent actionEvent) {
+        loadGameViewController.showScene();
+    }
+
+    @FXML
+    public void onHighscoresAction(ActionEvent actionEvent) {
+        highscoresViewController.showScene();
+    }
+}
