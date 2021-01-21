46f77ec780c09fb7cd7a0af0697acedec043d454
Abdullah Ourfali <abdullah.ourfali@tu-dortmund.de>
2021-01-21 14:23:08 +0100

    PlayerTest complete

--- tests/model/PlayerTest.java
+++ tests/model/PlayerTest.java
@@ -14,10 +14,13 @@ public class PlayerTest {
     private final String NAME = "Ad";
     private final QuiltBoard QUILTBOARD = new QuiltBoard();
     private final Score SCORE;
+    //private final int MONEY2 = 2;
+    //private final String NAME2 = "Ab";
     {
         SCORE = new Score(5, true, PlayerType.HUMAN, "Ad");
     }
     private final Player PLAYER = new Player(BOARDPOSIZION, MONEY, NAME, PlayerType.AI_MEDIUM, false);
+    //private final Player PLAYER2 = new Player(BOARDPOSIZION, MONEY2, NAME2, PlayerType.AI_MEDIUM, false);
 
     /**
      * Tests the Constructor
@@ -82,4 +85,59 @@ public class PlayerTest {
         PLAYER.setScore(SCORE);
         assertEquals(PLAYER.getScore(), SCORE);
     }
+
+    /**
+     * tests setMoney()
+     */
+    @Test
+    public void testSetMoney(){
+        PLAYER.setMoney(2);
+        assertEquals(PLAYER.getMoney(),3);
+    }
+
+    /**
+     * Tests setName()
+     */
+    @Test
+    public void testSetName(){
+        PLAYER.setName("Ac");
+        assertEquals(PLAYER.getName(), "Ac");
+    }
+
+    /**
+     * Tests setMinusMoney()
+     */
+    @Test
+    public void testSetMinusMoney(){
+        PLAYER.setMinusMoney(1);
+        assertEquals(PLAYER.getMoney(),0);
+    }
+
+    /**
+     * Tests setBoardPosition()
+     */
+    @Test
+    public void testSetBoardPosition(){
+        PLAYER.setBoardPosition(5);
+        assertEquals(PLAYER.getBoardPosition(), 5);
+    }
+
+    /**
+     * Tests setPlayerType()
+     */
+    @Test
+    public void testSetPlayerType(){
+        PLAYER.setPlayerType(PlayerType.AI_HARD);
+        assertEquals(PLAYER.getPlayerType(), PlayerType.AI_HARD);
+    }
+
+    /**
+     * Tests setHasSpecialTile() and getHasSpecialTile()
+     */
+    @Test
+    public void testgetHasSpecialTile(){
+        PLAYER.setHasSpecialTile(true);
+        assertEquals(PLAYER.getHasSpecialTile(), true);
+    }
+
 }
