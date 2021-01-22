0fa9fb44a5c273049b4727a317c760349e1f04fe
YannickNaumann <yannick.naumann@tu-dortmund.de>
2021-01-22 10:49:13 +0100

    model/PlayerType JavaDoc

--- src/model/PlayerType.java
+++ src/model/PlayerType.java
@@ -1,13 +1,28 @@
 package model;
 
+/**
+ * The type of the player
+ */
 public enum PlayerType {
 
+	/**
+	 * Player is an AI with difficulty easy
+	 */
 	AI_EASY,
 
+	/**
+	 * Player is an AI with difficulty medium
+	 */
 	AI_MEDIUM,
 
+	/**
+	 * Player is an AI with difficulty hard
+	 */
 	AI_HARD,
 
+	/**
+	 * Player is a Human
+	 */
 	HUMAN;
 
 }
