d6b44090b6a9e311512826c57f83ad74e1596328
Lukas <lukas.kidin@udo.edu>
2021-02-03 05:13:42 +0100

    Removed unused stuff from hardAI

--- src/ai/HardAI.java
+++ src/ai/HardAI.java
@@ -3,9 +3,7 @@ package ai;
 import model.*;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
