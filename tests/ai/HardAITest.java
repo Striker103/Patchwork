638095d51216de3db6baca139c9137e6389c606a
sopr097 <yannick.naumann@tu-dortmund.de>
2021-02-09 20:12:35 +0100

    Fixed HardAITest

--- tests/ai/HardAITest.java
+++ tests/ai/HardAITest.java
@@ -1,9 +1,6 @@
 package ai;
 
-import model.Matrix;
-import model.Patch;
-import model.QuiltBoard;
-import model.Tuple;
+import model.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,6 +12,7 @@ public class HardAITest {
     private QuiltBoard qb;
     private Patch x11;
     private HardAI ai;
+    private Player player;
 
     /**
      * Initializes test
@@ -23,6 +21,7 @@ public class HardAITest {
     public void init(){
         boolean[][] shape = {{true,false,false,false,false}, {false,false,false,false,false}, {false,false,false,false,false}};
         x11 = new Patch(1, 1, 1, new Matrix(shape), 1);
+        player = new Player("AI",PlayerType.AI_HARD,new Score(-157,false,PlayerType.AI_EASY,"EasyAI"));
         qb = new QuiltBoard();
         ai = new HardAI();
     }
@@ -32,7 +31,7 @@ public class HardAITest {
      */
     @Test
     public void testPlacing1x1(){
-        Tuple<QuiltBoard, Double> tuple = ai.placePatch(qb, x11);
+        Tuple<QuiltBoard, Double> tuple = ai.placePatch(qb, x11,player);
         System.out.println(tuple.getSecond());
     }
 }
